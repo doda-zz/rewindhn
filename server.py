64f2daa0749024d7437bf72969426c4c20c4a220
Dominik Dabrowski <dominik@silberrock.com>
2012-10-20 12:06:31 +0200

    remove template loading and debug statement

--- server.py
+++ server.py
@@ -4,18 +4,12 @@ import bson
 
 from ddd import IN_DEVELOPMENT
 from datetime import datetime
-import os
 import json
 
 app = Flask(__name__)
 DB = pymongo.Connection().hnmod.cleaned
 DB.create_index('idx')
 ACCEPTED_ARGS = set(('spec', 'fields', 'skip', 'limit', 'sort'))
-
-with open(os.path.expanduser('~/pr/rewind/static/rewind/js/templates/page.ejs')) as f:
-    page_tpl = f.read()
-with open(os.path.expanduser('~/pr/rewind/static/rewind/js/templates/post.ejs')) as f:
-    post_tpl = f.read()
 
 class MongoEncoder(json.JSONEncoder):
     def default(self, obj):
@@ -31,7 +25,6 @@ def jsonify(*args, **kwargs):
 
 @app.route('/api/v1/pages')
 def api():
-    print request.args
     args = dict((k, json.loads(v)) for k,v in request.args.items() if k in ACCEPTED_ARGS)
     args['limit'] = min(200, args.get('limit', 30))
     args.setdefault('fields', {'_id': False, 'html': False})
@@ -43,9 +36,7 @@ def home():
     last_page = next(DB.find({'page':0}, {'_id':False, 'html':False}).sort('created_at', -1).limit(1))
     max_page_idx = last_page['idx']
     return render_template('base.html',
-        pages={ max_page_idx: last_page },
-        post_tpl=post_tpl,
-        page_tpl=page_tpl)
+        pages={ max_page_idx: last_page })
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8010, debug=IN_DEVELOPMENT)
