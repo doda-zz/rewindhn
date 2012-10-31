1e2f88078ce2b2b90644b5fdec45b8cc297dddbf
Dominik Dabrowski <dominik@silberrock.com>
2012-10-31 17:58:02 +0100

    safe writes

--- db.py
+++ db.py
@@ -1,7 +1,7 @@
 import pymongo
 from devel import IN_DEVELOPMENT
 if IN_DEVELOPMENT:
-    CONN = pymongo.Connection()
+    CONN = pymongo.Connection(safe=True)
 else:
-    CONN = pymongo.Connection(port=29919)
+    CONN = pymongo.Connection(port=29919, safe=True)
 DB = CONN.rewind
